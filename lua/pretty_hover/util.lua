0151f30830481dd3b466b5cd23ca8bf713e6e114
Filip Lobpreis <f.lobpreis@gmail.com>
2023-04-09 20:31:54 +0200

    util: edge case of \param
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/pretty_hover/util.lua
+++ lua/pretty_hover/util.lua
@@ -31,6 +31,26 @@ M.joint_table = function(tbl, delim)
 	return result
 end
 
+--- Check if a table contains desired element. vim.tbl_contains does not work for all cases.
+---@param tbl table Table to be checked.
+---@param el string Element to be checked.
+---@return boolean True if the table contains the element, false otherwise.
+M.tbl_contains = function(tbl, el)
+	if not el then
+		return false
+	end
+	if not tbl then
+		return false
+	end
+
+	for _, v in pairs(tbl) do
+		if el:find(v) then
+			return true
+		end
+	end
+	return false
+end
+
 --- Converts a string returned by response.result.contents.value from vim.lsp[textDocument/hover] to markdown.
 ---@param toConvert string Documentation of the string to be converted.
 ---@param opts table Table of options to be used for the conversion to the markdown language.
@@ -43,21 +63,22 @@ M.convert_to_markdown = function(toConvert, opts)
 
 	for _, chunk in pairs(chunks) do
 		local el = M.split(chunk)[1]
-		if vim.tbl_contains(opts.line, el) then
+
+		if M.tbl_contains(opts.line, el) then
 			local tbl = M.split(chunk)
 			table.remove(tbl, 1)
 			chunk = M.joint_table(tbl, " ")
 			table.insert(result, opts.stylers.line .. chunk .. opts.stylers.line)
 			table.insert(result, "")
 
-		elseif vim.tbl_contains(opts.header, el) then
+		elseif M.tbl_contains(opts.header, el) then
 			local tbl = M.split(chunk)
 			table.remove(tbl, 1)
 			chunk = M.joint_table(tbl, " ")
 			table.insert(result, opts.stylers.header .. " " .. chunk)
 			table.insert(result, "")
 
-		elseif vim.tbl_contains(opts.word, el) then
+		elseif M.tbl_contains(opts.word, el) then
 			local tbl = M.split(chunk)
 			local operation = tbl[1]
 			tbl[2] = opts.stylers.word .. tbl[2] .. opts.stylers.word
