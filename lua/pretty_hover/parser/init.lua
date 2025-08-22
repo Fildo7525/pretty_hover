abed626f375de0e57ce72d8c1704701a9fe0a64f
Filip Lobpreis <f.lobpreis@gmail.com>
2025-08-22 22:20:39 +0200

    fix(#34): parser: return empty ParserOutput on empty input
    If the input to the parse function is nil / emtpy string / emtpy table,
    an emtpy ParserOutput object will be returned. Meaning the text and the
    highlighting fields will be set to empty tables.
    
    To check if the returned object is emtpy you can do the following
    local out = parser.parse(nil)
    vim.print(out == out:emtpy() and "Equal" or "Different")
    
    The out:empty() creates a new empty object which can be used for
    comparison. The __eq method is the only comparison method implemented.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/pretty_hover/parser/init.lua
+++ lua/pretty_hover/parser/init.lua
@@ -6,14 +6,26 @@ local parser = require("pretty_hover.parser.parser")
 ---@field public text table
 ---@field public highlighting table
 ---@field public new fun(self, text: table, highlighting: table): ParserOutput
+---@field public empty fun(self): ParserOutput
 ---@field public string fun(self): string
-ParserOutput = {
+local ParserOutput = {
 	text = {},
 	highlighting = {},
 	string = function(self)
 		return table.concat(self.text, "\n")
 	end,
 }
+
+--- Comparator function to check if two ParserOutput structs are equal.
+---@param lhs ParserOutput
+---@param rhs ParserOutput
+---@return boolean Result whether the lhs and rhs sides of the comparison are the same
+local function ParserOutputEQ(lhs, rhs)
+	local str_lhs = vim.inspect(lhs)
+	local str_rhs = vim.inspect(rhs)
+
+	return vim.fn.sha256(str_lhs) == vim.fn.sha256(str_rhs)
+end
 
 --- Creates new ParserOutput object.
 ---
@@ -43,21 +55,27 @@ ParserOutput = {
 ---@return ParserOutput out New object of the referenced type
 function ParserOutput:new(text, highlighting)
 	local out = {}
-	setmetatable(out, { __index = self })
+	setmetatable(out, { __index = self, __eq = ParserOutputEQ })
 	out.text = text
 	out.highlighting = highlighting
 	return out
 end
 
+function ParserOutput:empty()
+	return ParserOutput:new({}, {})
+end
+
 --- @brief This method parses the input string or table and converts the contents from doxygen into markdown format.
 ---
 --- NOTE: The string must have new lines inside. If the string is not separated by them the parsing will not be done.
+--- Additionally, if nil, empty string or empty table are passed in the returned object will have empty text and highlighting
+--- fields.
 ---
---- The output highlighting data can be applied fx. like this
+--- The output highlighting data can be applied fx. this
 ---
 --- ```lua
 --- function M.apply_highlight(config, hl_data, bufnr)
---- 	if M.hl_ns then
+---ll 	if M.hl_ns then
 --- 		api.nvim_buf_clear_namespace(bufnr, M.hl_ns, 0, -1)
 --- 	end
 ---
@@ -82,6 +100,13 @@ end
 --- @overload fun(text: string): ParserOutput
 --- @overload fun(text: table): ParserOutput
 function M.parse(text)
+	if not text
+	   or (type(text) == "string" and text == "")
+	   or (type(text) == "table" and vim.tbl_isempty(text))
+	then
+		return ParserOutput:empty()
+	end
+
 	local config = require("pretty_hover.config"):instance()
 
 	local hl_data = {
